6c74d26e2182a8690484ec2fd026b262b650e7e5
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-07-13 23:05:19 +0200

    feat: implement clock hand angle task

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  function getArea() {
+    return this.height * this.width;
+  }
+  return {
+    width,
+    height,
+    getArea,
+  };
 }
 
 
