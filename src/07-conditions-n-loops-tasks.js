8daf5b49b19918d2f1cf4f5f13a105689bd1cf69
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-07-13 13:55:55 +0200

    feat: implement promises #1-3

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -125,7 +125,7 @@ function isTriangle(a, b, c) {
  *
  */
 function doRectanglesOverlap(rect1, rect2) {
-  if (rect1.top < rect2.top + rect2.height || rect2.top < rect1.top + rect1.height) {
+  if (rect1.top > rect2.top + rect2.height || rect2.top > rect1.top + rect1.height) {
     return false;
   }
   if (rect1.left + rect1.width < rect2.left || rect2.left + rect2.width < rect1.left) {
